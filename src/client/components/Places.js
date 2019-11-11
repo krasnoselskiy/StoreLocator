27bc39a44a5f9d2d0cb156bca64b1c7f4c24cc9a
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2019-11-11 18:56:15 +0200

    Begin save functionality

--- src/client/components/Places.js
+++ src/client/components/Places.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Places = ({ places }) => (
-  <ul>
-    {places ? places.map((place, i) =>
-      <li key={i}>
-        <div>{place.display_name}</div>
-        <div>{place.lat}</div>
-        <div>{place.lon}</div>
-      </li>
-    ) : null}
-  </ul>
-)
+const Places = ({ places }) => {
+
+  const handlerToSavePin = (e) => {
+    console.log(e);
+  }
+
+  return (
+    <ul>
+      {places ? places.map((place, i) =>
+        <li key={i} className="list_item">
+          <div>{place.display_name}</div>
+          <div>{place.lat}</div>
+          <div>{place.lon}</div>
+
+          <button className="btn btn-info save_button" onClick={handlerToSavePin}>Save</button>
+        </li>
+      ) : null}
+    </ul>
+  )
+}
 
 Places.propTypes = {
   places: PropTypes.array.isRequired
