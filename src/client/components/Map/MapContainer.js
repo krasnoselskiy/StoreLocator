868b72dbeb62daa486df727660af9ddb170a4029
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2019-11-11 18:58:05 +0200

    Created pin in map component

--- src/client/components/Map/MapContainer.js
+++ src/client/components/Map/MapContainer.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { connect } from 'react-redux'
+
+import MapSidebar from './MapSidebar'
 
 import './map.css'
 
-export class MapContainer extends Component {
+const MapContainer = ({ places }) => {
+  const mapInitPosition = [];
+  mapInitPosition.push(places[0].lat, places[0].lon)
 
-  render() {
-    const position = [51.505, -0.09]
-    return (
-      <div className="mapContainer mt-5">
-        <Map center={position} zoom={13}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          />
-          <Marker position={position}>
-            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-          </Marker>
-        </Map>
-      </div>
-    )
+  return (
+    <div className="mapContainer mt-5">
+      <MapSidebar />
+      <Map center={mapInitPosition} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+
+        {places.map((point, index) => {
+          const point_position = []
+          const lat = point.lat;
+          const lon = point.lon;
+
+          point_position.push(lat, lon)
+
+          return (
+            <Marker key={index} position={point_position}>
+              <Popup>Doesn't show the text</Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    places: state.placesByAdress.places
   }
 }
 
-export default MapContainer
+export default connect(mapStateToProps)(MapContainer)
