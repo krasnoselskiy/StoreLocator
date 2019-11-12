dac6f1e71bba426b623e1ca5c447d3b3c8304c44
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2019-11-12 13:49:28 +0200

    Added loading file from client to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,33 @@
-const express = require('express');
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/mydb'
 
 const app = express();
-
+app.use(cors());
 app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage }).single('file')
+
+app.post('/upload', function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+    } else if (err) {
+      return res.status(500).json(err)
+    }
+    return res.status(200).send(req.file)
+  })
+});
