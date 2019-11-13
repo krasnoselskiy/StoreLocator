d509e937e5baafa0d4bd8ae04bace876132bd498
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2019-11-13 14:22:04 +0200

    Changed Places component in client side:   - logic moved to Place component

--- src/client/components/Places.js
+++ src/client/components/Places.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Place from './Place'
+
 const Places = ({ places }) => {
-
-  const handlerToSavePin = (e) => {
-    console.log(e);
-  }
-
   return (
     <ul>
       {places ? places.map((place, i) =>
-        <li key={i} className="list_item">
-          <div>{place.display_name}</div>
-          <div>{place.lat}</div>
-          <div>{place.lon}</div>
-
-          <button className="btn btn-info save_button" onClick={handlerToSavePin}>Save</button>
-        </li>
+        <Place key={i} place={place} />
       ) : null}
     </ul>
   )
