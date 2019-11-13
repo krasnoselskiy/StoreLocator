a0a71a184183d527ef1ff4f1fbc7f63a1a2af27e
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2019-11-13 14:22:21 +0200

    Added Place component in client side

---
+++ src/client/components/Place.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+const Place = (props) => {
+
+  const { place } = props;
+
+  const handlerToSavePin = (e) => {
+    const data = {
+      title: place.display_name,
+      lat: place.lat,
+      lon: place.lon
+    }
+
+    axios.post('http://localhost:5000/create', data, {})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  const place_title = `${place.display_name.substring(0, 85)}...`;
+
+  return (
+    <li className="list_item mb-2">
+      <div>{place_title}</div>
+
+      <button className="btn btn-info save_button" onClick={handlerToSavePin}>Save</button>
+    </li>
+  );
+}
+
+export default Place;
