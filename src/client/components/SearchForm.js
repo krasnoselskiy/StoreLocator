d2331c1f26dc41e48f3ac801eccdf164aed6d08c
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2019-11-21 19:40:35 +0200

    Added parsing csv file to json format and sending it to server for writting to the db

--- src/client/components/SearchForm.js
+++ src/client/components/SearchForm.js
@@ -3,25 +3,21 @@ import { connect } from 'react-redux'
 import CSVReader from 'react-csv-reader'
 import axios from 'axios';
 
-import { fetchPlaces, openMapColumn } from '../redux/actions'
+import { fetchPlaces, openMapColumn, fetchPlacesFromDB } from '../redux/actions'
 
 export class SearchForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedFile: null
+      stores: []
     }
-
-    this.handleLoadingFile = this.handleLoadingFile.bind(this);
   }
 
   handleGettingPlaces = e => {
     e.preventDefault()
     const { dispatch } = this.props
-    const event_parent = e.target.parentNode;
-    const place_input = event_parent.querySelector(".place-input")
-    let address = place_input.value;
+    let address = e.target.value;
 
     if (address) {
       dispatch(fetchPlaces(address))
@@ -29,42 +25,39 @@ export class SearchForm extends Component {
     }
   }
 
-  handleLoadingFile = e => {
-    const file = event.target.files[0];
+  createObj = (first, second) => {
+    const result = {};
 
-    this.setState({
-      selectedFile: file
+    first.forEach((key, i) => {
+      key = key.replace(/ /g, '');
+      let value = second[i];
+      return result[key] = value;
     })
+
+    return result;
   }
 
   handleCreateJSON = (data) => {
     let json = [];
 
-    function createObj(first, second) {
-      const result = {};
-      first.forEach((key, i) => {
-        let value = second[i].replace(/['"]+/g, '').replace(/\s+/g, '');
-        return result[key] = value;
-      })
-      return result;
-    }
-
     data.map((item, i) => {
-      if (i > 0) {
-        json.push(createObj(data[0], data[i]))
+      if (i > 0 && item[0].length) {
+        json.push(this.createObj(data[0], data[i]))
       }
     })
 
-    console.log(json);
+    this.setState({
+      stores: json
+    })
   }
 
   handleLoadToServerFile = e => {
-    const data = new FormData();
-    data.append('file', this.state.selectedFile)
+    const { dispatch } = this.props
+    const data = this.state.stores;
 
     axios.post("http://localhost:5000/upload", data, {})
       .then(res => {
-        console.log(res.statusText)
+        dispatch(fetchPlacesFromDB())
       })
   }
 
@@ -72,18 +65,16 @@ export class SearchForm extends Component {
     return (
       <div className="row" >
         <div className="form-wrap col-6">
-          <form onSubmit={this.handleGettingPlaces} className="form-group">
+          <form className="form-group">
             <div className="form-group">
-              <input className="form-control place-input" type="text" placeholder="Enter store addess" />
+              <input className="form-control place-input" type="text" onChange={this.handleGettingPlaces} placeholder="Enter store addess" />
             </div>
-            <button type="submit" className="btn btn-info"
-            >Find address</button>
           </form>
         </div>
 
         <div className="col-6">
           <CSVReader onFileLoaded={data => this.handleCreateJSON(data)} />
-          {this.state.selectedFile ?
+          {this.state.stores.length ?
             <div className="form-group">
               <button type="button" className="btn btn-info btn-block" onClick={this.handleLoadToServerFile}>Upload</button>
             </div> : null}
